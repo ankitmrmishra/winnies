1bfa1bd2fa073552f7c3605e67cc289ef7134a2a
ankitmrmishra <ankitmrmishra1118@gmail.com>
2025-03-22 15:47:59 +0530

    removed holi popup

--- components/PackagePopup.tsx
+++ components/PackagePopup.tsx
@@ -1,23 +1,21 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { X } from "lucide-react";
-// import { motion } from "framer-motion";
+import { Users, ChevronLeft, ChevronRight } from "lucide-react";
+import { motion } from "framer-motion";
 import {
   Dialog,
   DialogContent,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
-
 import { Button } from "@/components/ui/button";
-// import { packages } from "@/data/packages";
-// import { useRouter } from "next/navigation";
-import HoliPopoUp from "../public/holipng.png";
-import Image from "next/image";
+import { packages } from "@/data/packages";
+import { useRouter } from "next/navigation";
+
 export function PackageModal() {
   const [isOpen, setIsOpen] = useState(false);
-  // const [currentPackageIndex, setCurrentPackageIndex] = useState(0);
+  const [currentPackageIndex, setCurrentPackageIndex] = useState(0);
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -27,35 +25,35 @@ export function PackageModal() {
     return () => clearTimeout(timer);
   }, []);
 
-  // const nextPackage = () => {
-  //   setCurrentPackageIndex((prevIndex) => (prevIndex + 1) % packages.length);
-  // };
+  const nextPackage = () => {
+    setCurrentPackageIndex((prevIndex) => (prevIndex + 1) % packages.length);
+  };
 
-  // const prevPackage = () => {
-  //   setCurrentPackageIndex(
-  //     (prevIndex) => (prevIndex - 1 + packages.length) % packages.length
-  //   );
-  // };
+  const prevPackage = () => {
+    setCurrentPackageIndex(
+      (prevIndex) => (prevIndex - 1 + packages.length) % packages.length
+    );
+  };
 
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
-      <DialogContent className="sm:max-w-max  bg-white/40 max-h-[100vh] overflow-y-auto">
+      <DialogContent className="sm:max-w-[900px] bg-white max-h-[90vh] overflow-y-auto">
         <DialogHeader>
           <DialogTitle className="text-4xl text-center text-emerald-800 italic">
-            Holi Special!!!
+            Special Packages
           </DialogTitle>
-          <Button
+          {/* <Button
             variant="ghost"
-            className="absolute right-4 top-2 rounded-full bg-emerald-100 text-emerald-800 hover:bg-emerald-200 hover:text-emerald-900 z-10"
+            className="absolute right-4 top-4 rounded-full bg-emerald-100 text-emerald-800 hover:bg-emerald-200 hover:text-emerald-900 z-10"
             onClick={() => setIsOpen(false)}
           >
             <X className="h-6 w-6" />
             <span className="sr-only">Close</span>
-          </Button>
+          </Button> */}
         </DialogHeader>
         <div className="py-6">
           {/* Mobile view */}
-          {/* <div className="block sm:hidden">
+          <div className="block sm:hidden">
             <PackageCard pkg={packages[currentPackageIndex]} />
             <div className="flex justify-between mt-4">
               <Button
@@ -75,79 +73,70 @@ export function PackageModal() {
                 <ChevronRight className="h-4 w-4 ml-2" />
               </Button>
             </div>
-          </div> */}
-
-          <Image
-            alt=""
-            src={HoliPopoUp}
-            width={500}
-            height={500}
-            className="w-96 h-full object-contain"
-          />
+          </div>
 
           {/* Desktop view */}
-          {/* <div className="hidden sm:grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          <div className="hidden sm:grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
             {packages.map((pkg, index) => (
               <PackageCard key={index} pkg={pkg} />
             ))}
-           
-          </div> */}
+          </div>
         </div>
       </DialogContent>
     </Dialog>
   );
 }
-// export interface Package {
-//   title: string;
-//   price: string;
-//   occupancy: string;
-//   description: string[];
-// }
+export interface Package {
+  title: string;
+  price: string;
+  occupancy: string;
+  description: string[];
+}
 
-// function PackageCard({ pkg }: { pkg: Package }) {
-//   const router = useRouter();
-//   return (
-//     <motion.div
-//       initial={{ opacity: 0, y: 20 }}
-//       animate={{ opacity: 1, y: 0 }}
-//       transition={{ duration: 0.6 }}
-//       className="bg-white p-4 sm:p-6 rounded-lg shadow-lg border border-emerald-100 flex flex-col justify-between"
-//     >
-//       <div>
-//         <h3 className="text-xl font-semibold mb-4 text-emerald-800 italic">
-//           {pkg.title}
-//         </h3>
-//         <div className="flex md:flex-col flex-row items-start justify-between mb-4">
-//           <div className="flex items-center mb-2 sm:mb-0">
-//             <span className="text-2xl sm:text-3xl font-bold text-emerald-800">
-//               ₹{pkg.price}
-//             </span>
-//             <span className="text-gray-600 ml-2">/ package</span>
-//           </div>
-//           <div className="flex items-center">
-//             <Users className="w-5 h-5 text-emerald-600 mr-2" />
-//             <span className="text-gray-600">{pkg.occupancy}</span>
-//           </div>
-//         </div>
-//         <ul className="space-y-2 mb-6">
-//           {pkg.description.map((item, i) => (
-//             <li key={i} className="flex items-start">
-//               <span className="text-emerald-600 mr-2">•</span>
-//               <span className="text-gray-600">{item}</span>
-//             </li>
-//           ))}
-//         </ul>
-//       </div>
-//       <Button
-//         onClick={() =>
-//           router.push(
-//             "https://bookings.resavenue.com/resBooking/availsearch?regCode=GATE0402"
-//           )
-//         }
-//         className="w-full bg-emerald-800 hover:bg-emerald-700 text-white"
-//       >
-//         Book Now
-//       </Button>
-//     </motion.div>
-//   );
-// }
+function PackageCard({ pkg }: { pkg: Package }) {
+  const router = useRouter();
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.6 }}
+      className="bg-white p-4 sm:p-6 rounded-lg shadow-lg border border-emerald-100 flex flex-col justify-between"
+    >
+      <div>
+        <h3 className="text-xl font-semibold mb-4 text-emerald-800 italic">
+          {pkg.title}
+        </h3>
+        <div className="flex md:flex-col flex-row items-start justify-between mb-4">
+          <div className="flex items-center mb-2 sm:mb-0">
+            <span className="text-2xl sm:text-3xl font-bold text-emerald-800">
+              ₹{pkg.price}
+            </span>
+            <span className="text-gray-600 ml-2">/ package</span>
+          </div>
+          <div className="flex items-center">
+            <Users className="w-5 h-5 text-emerald-600 mr-2" />
+            <span className="text-gray-600">{pkg.occupancy}</span>
+          </div>
+        </div>
+        <ul className="space-y-2 mb-6">
+          {pkg.description.map((item, i) => (
+            <li key={i} className="flex items-start">
+              <span className="text-emerald-600 mr-2">•</span>
+              <span className="text-gray-600">{item}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <Button
+        onClick={() =>
+          router.push(
+            "https://bookings.resavenue.com/resBooking/availsearch?regCode=GATE0402"
+          )
+        }
+        className="w-full bg-emerald-800 hover:bg-emerald-700 text-white"
+      >
+        Book Now
+      </Button>
+    </motion.div>
+  );
+}
